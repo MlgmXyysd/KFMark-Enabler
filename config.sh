2f5828f8efb07fea0afa84e336dee5eb09c09be3
Yuze Wu <1204074991@qq.com>
2018-12-23 22:59:35 +0800

    v1.0.0

---
+++ config.sh
@@ -0,0 +1,13 @@
+AUTOMOUNT=true
+PROPFILE=false
+POSTFSDATA=true
+LATESTARTSERVICE=true
+print_modname() {
+  ui_print "********************"
+  ui_print "*  KFMark Enabler  *"
+  ui_print "********************"
+}
+REPLACE=""
+set_permissions() {
+  set_perm_recursive  $MODPATH  0  0  0755  0644
+}
