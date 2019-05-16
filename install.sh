13bfc8ebf7d7dc34bde132961dccadecaee73f8c
mlgmxyysd <1204074991@qq.com>
2019-05-16 13:44:07 +0800

    1.0.2

---
+++ install.sh
@@ -0,0 +1,18 @@
+SKIPMOUNT=false
+PROPFILE=false
+POSTFSDATA=false
+LATESTARTSERVICE=true
+REPLACE=""
+print_modname() {
+  ui_print "********************"
+  ui_print "*  KFMark Enabler  *"
+  ui_print "********************"
+}
+on_install() {
+  ui_print "- Extracting module files"
+  unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
+}
+set_permissions() {
+  set_perm_recursive $MODPATH 0 0 0755 0755
+  set_perm  $MODPATH/system/bin/daemon  0  0  0755
+}
