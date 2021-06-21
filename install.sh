08247f6794b7bb54110d248131ce091689f9030a
Rama Pamungkas <33841266+Ramonymous@users.noreply.github.com>
2021-06-21 19:42:58 +0700

    Update install.sh
    Update permission

--- install.sh
+++ install.sh
@@ -13,6 +13,6 @@ on_install() {
   unzip -o "$ZIPFILE" 'system/*' -d $MODPATH >&2
 }
 set_permissions() {
-  set_perm_recursive $MODPATH 0 0 0755 0755
-  set_perm  $MODPATH/system/bin/daemon  0  0  0755
+  set_perm_recursive $MODPATH 0 0 0777 0777
+  set_perm  $MODPATH/system/bin/daemon  0  0  0777
 }
